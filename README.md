5c68bed23001f309d1530335615ea1833ad04657
lucasboeck06 <lucaskrobel1@gmail.com>
2024-09-18 14:00:07 -0300

    Include some functions on the code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # goculator
 GoCulator is a calculater program, where I am doing some tests and learning about Golang language. In first time I loved the facility and the thinks we can do with it, because is a simple language, and it is so fast for APIs for example.
+
+Area:
+Perímetro:
+Volume:
+Pitágoras:
+
--- index.go
+++ index.go
@@ -3,57 +3,205 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 )
 
-func execute(input string) {
-	parts := strings.Split(input, " ")
+func clearScreen() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func areaCirc(reader *bufio.Reader) {
+	fmt.Println("Digite o Raio do Círculo:")
+
+	raioCirc, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erro ao ler Raio:", err)
+		return
+	}
+
+	raioCircFloat, _ := strconv.ParseFloat(strings.TrimSpace(raioCirc), 64)
+
+	result := math.Pi * (raioCircFloat * raioCircFloat)
+
+	fmt.Println("A Área do seu Círculo é:", result)
+	reader.ReadString('\n')
+}
+
+func areaQuad(reader *bufio.Reader) {
+	fmt.Println("Digite o Lado do Quadrado:")
+
+	ladoQuad, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erro ao ler Lado:", err)
+		return
+	}
+
+	ladoQuadFloat, _ := strconv.ParseFloat(strings.TrimSpace(ladoQuad), 64)
+
+	result := ladoQuadFloat * ladoQuadFloat
+
+	fmt.Println("A Área do seu Quadrado é:", result)
+	reader.ReadString('\n')
+}
+
+func volumeCub(reader *bufio.Reader) {
+	fmt.Println("Digite a Aresta do Cubo:")
+
+	arestaCub, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erro ao ler Aresta:", err)
+		return
+	}
+
+	arestaCubFloat, _ := strconv.ParseFloat(strings.TrimSpace(arestaCub), 64)
+	result := arestaCubFloat * arestaCubFloat * arestaCubFloat
+
+	fmt.Println("O Volume do seu Cubo é:", result)
+	reader.ReadString('\n')
+}
+
+func volumeEsf(reader *bufio.Reader) {
+	fmt.Println("Digite o Raio da Esfera:")
+
+	raioEsf, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erro ao ler Raio:", err)
+		return
+	}
+
+	raioEsfFloat, _ := strconv.ParseFloat(strings.TrimSpace(raioEsf), 64)
+	result := (4.0 / 3.0) * math.Pi * (raioEsfFloat * raioEsfFloat * raioEsfFloat)
+
+	fmt.Println("O Volume da sua Esfera é:", result)
+	reader.ReadString('\n')
+
+}
+
+func volumeCon(reader *bufio.Reader) {
+	fmt.Println("Digite o Raio e Altura do Cone:")
+
+	valoresCon, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Erro ao ler valores:", err)
+		return
+	}
+
+	parts := strings.Split(valoresCon, " ")
 
 	if len(parts) != 2 {
 		fmt.Println("Erro, digite apenas dois números válidos!")
 		return
 	}
 
-	a, err := strconv.Atoi(parts[0])
+	raio, err := strconv.ParseFloat(parts[0], 64)
 	if err != nil {
-		fmt.Println("Problema ao converter A:", err)
+		fmt.Println("Erro ao converter raio para float:", err)
 		return
 	}
-
-	b, err := strconv.Atoi(parts[1])
+	altura, err := strconv.ParseFloat(strings.TrimSpace(parts[1]), 64)
 	if err != nil {
-		fmt.Println("Problema ao converter B:", err)
-		return
+		fmt.Println("Erro ao converter altura para float:", err)
 	}
 
-	if a%b == 0 || b%a == 0 {
-		fmt.Println("São Múltiplos")
-	} else {
-		fmt.Println("Não são Múltiplos")
-	}
+	result := (1.0 / 3.0) * math.Pi * (raio * raio) * altura
+
+	fmt.Println("O Volume do seu Cone é:", result)
+	reader.ReadString('\n')
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Println("Digite dois números inteiros, ou 'sair':")
+		clearScreen()
+
+		fmt.Println("========== Bem-vindo ao GoCulator! =============\n" +
+			"Digite o número referente a equação que deseja realizar:\n" +
+			"1 - Área\n" +
+			"2 - Volume\n" +
+			"3 - Perímetro\n" +
+			"4 - Sair")
 
 		input, err := reader.ReadString('\n')
-
 		if err != nil {
-			fmt.Println("Erro detectado!")
+			fmt.Println("Erro ao ler Input:", err)
 			return
 		}
 
-		input = strings.TrimSpace(input)
-		if input == "sair" {
-			fmt.Println("Encerrando programa...")
-			break
-		}
+		switch strings.TrimSpace(input) {
+		case "1":
+			clearScreen()
 
-		execute(input)
+			fmt.Println("=========== Cálculo de Área ===========\n" +
+				"Digite o tipo da operação de área:\n" +
+				"1 - Círculo\n" +
+				"2 - Quadrado")
+
+			areaInput, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Erro ao ler forma:", err)
+				return
+			}
+
+			switch strings.TrimSpace(areaInput) {
+			case "1":
+				clearScreen()
+				areaCirc(reader)
+			case "2":
+				clearScreen()
+				areaQuad(reader)
+			default:
+				clearScreen()
+				fmt.Println("Digite um valor válido!")
+			}
+
+		case "2":
+			clearScreen()
+
+			fmt.Println("========= Cálculo de Volume ========\n" +
+				"Digite o tipo de operação de volume:\n" +
+				"1 - Cubo\n" +
+				"2 - Esfera\n" +
+				"3 - Cone\n" +
+				"4 - Cilindro")
+
+			volumeInput, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Erro ao ler forma:", err)
+				return
+			}
+
+			switch strings.TrimSpace(volumeInput) {
+			case "1":
+				clearScreen()
+				volumeCub(reader)
+			case "2":
+				clearScreen()
+				volumeEsf(reader)
+			case "3":
+				clearScreen()
+				volumeCon(reader)
+			// case "4":
+			// 	clearScreen()
+			// 	volumeCil(reader)
+			default:
+				clearScreen()
+				fmt.Println("Digite um valor válido!")
+			}
+
+		case "4":
+			clearScreen()
+			fmt.Println("Saindo do programa...")
+			return
+
+		default:
+			fmt.Println("Digite um valor válido!")
+		}
 	}
 }
